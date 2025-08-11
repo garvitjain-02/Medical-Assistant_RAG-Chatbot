eec25ee6fa6ed9f39f37e86e68bf5ab7a356ab28
Garvit Jain <garvitjainjnv@gmail.com>
2025-08-11 14:29:40 +0530

    updated the loading function

--- server/modules/load_vectorstore.py
+++ server/modules/load_vectorstore.py
@@ -41,3 +41,42 @@ if PINECONE_INDEX_NAME not in existing_indexes:
 index = pc.Index(PINECONE_INDEX_NAME)
 
 # load,split,embed and upsert pdf docs content
+
+def load_vectorstore(uploadedfiles):
+    embed_model = GoogleGenerativeAIEmbeddings(model="models/embedding-003")
+    file_path=[]
+
+    # 1. upload 
+    for file in uploadedfiles:
+        save_path=Path(UPLOAD_DIR)/file.file_name
+        with open(save_path, "wb") as f:
+            f.write(file.file.read())
+
+        file_path.append(str(save_path))
+
+    # 2. split
+
+    for file in file_path:
+        loader=PyPDFLoader(file)
+        documents=loader.load()
+
+        splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+        chunks=splitter.split_documents(documents)
+
+        text = [chunk.page_content for chunk in chunks]
+        metadata = [chunk.metadata for chunk in chunks]
+        ids = [f"{Path(file).stem}--{i}" for i in range(len(chunks))]
+
+        # 3. Embedding
+        print(f"Embedding {len(chunks)} Chunks")
+        embedding = embed_model.embed_documents(text)
+
+        # 4. Upsert in Pinecone
+        print(f"Upserting Embeddings")
+        with tqdm(total=len(embedding), desc="Upserting to pinecone") as progress:
+            index.upsert(vectors=zip(ids,embedding,metadata))
+            progress.update(len(embedding))
+
+
+        print(f"Finished Upserting {len(embedding)} Embeddings")
+        print(f"Upload complete for {file}")
