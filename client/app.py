03c0960a320ca3f9cbc8b9c258e427e794625de4
Garvit Jain <garvitjainjnv@gmail.com>
2025-08-13 16:57:09 +0530

    added render functions

--- client/app.py
+++ client/app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from components.chatUI import render_chat
+from components.upload import render_uploader
+from components.history_download import render_history_download 
 
 st.set_page_config(
     page_title="Medical Assistant",
@@ -6,3 +9,7 @@ st.set_page_config(
     layout="wide"
 )
 st.title("⚕️ Medical Assistant Chatbot")
+
+render_uploader()
+render_chat()
+render_history_download()
