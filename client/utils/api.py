55c0d93a722c51cd1ab6bec4d76f8e82f67b002b
Garvit Jain <garvitjainjnv@gmail.com>
2025-08-13 16:58:00 +0530

    updated upload pdf api functions

--- client/utils/api.py
+++ client/utils/api.py
@@ -2,9 +2,11 @@ import requests
 from config import API_URL
 
 def upload_pdfs_api(files):
-    files_payload=[("files", (f.name, f.read(), "application/pdf")) for f in files]
-    return requests.post(f"{API_URL}/upload_pdfs/", files=files_payload)
+    if not isinstance(files, list):
+        files = [files]
+    files_payload = [("files", (f.name, f.read(), "application/pdf")) for f in files]
+    return requests.post(f"{API_URL}/upload_pdf", files=files_payload)
 
 def ask_questions_api(question):
     payload = {"question": question}
-    return requests.post(f"{API_URL}/ask_questions/",data=payload) 
+    return requests.post(f"{API_URL}/ask/", data=payload)
